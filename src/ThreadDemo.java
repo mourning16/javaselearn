8230b8a6e2627985d74be5ea879f27e76fae7659
陶清晨 <776404387@qq.com>
2020-12-31 14:42:40 +0800

    #java se学习

--- src/ThreadDemo.java
+++ src/ThreadDemo.java
@@ -1,3 +1,5 @@
+import java.util.concurrent.TimeUnit;
+
 /**
  * @Description TODO
  * @Author Mourning 16
@@ -5,25 +7,63 @@
  * @Version 1.0
  */
 
+//wait和notify的练习
 public class ThreadDemo {
 
-    public static void main(String[] args){
-        System.out.println(Thread.currentThread().getId());
-        System.out.println(Thread.currentThread().getName());
-        new Thread(){
-            @Override
-            public void run(){
-                System.out.println("~~~~~~");
-                System.out.println(Thread.currentThread().getId());
-                System.out.println(Thread.currentThread().getName());
-            }
-        }.start();
+        public static String str = "init";
 
-        for(int i=0;i<10;i++){
-            Thread t = new Thread(new ThreadTask());
-            t.start();
+        public static void waitDemo(){
+
+            synchronized (str){
+                try {
+                    str.wait();
+                    System.out.println("线程" + Thread.currentThread().getName() + "被阻塞");
+                } catch (InterruptedException e) {
+                    System.out.println("str的阻塞被打断");
+                }
+            }
         }
 
-    }
+        public static void notifyDemo(){
+            synchronized (str){
+                str.notify();
+                System.out.println(Thread.currentThread().getName() + "已经被打唤醒");
+            }
+        }
 
+}
+
+class Test{
+
+    private static final int MAX_COUNT = 10;
+
+    public static void main(String[] args) {
+        new Thread(new WaitThread()).start();
+        new Thread(new NotifyThread()).start();
+    }
+}
+
+class WaitThread implements Runnable{
+
+    @Override
+    public void run() {
+        System.out.println(Thread.currentThread().getName() + "线程被启动");
+        System.out.println("即将阻塞线程:"+Thread.currentThread().getName());
+        ThreadDemo.waitDemo();
+    }
+}
+
+class NotifyThread implements Runnable{
+
+    @Override
+    public void run() {
+        System.out.println(Thread.currentThread().getName() + "线程被启动");
+        try {
+            System.out.println("沉睡十秒后即将唤醒线程~~");
+            TimeUnit.SECONDS.sleep(10);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        ThreadDemo.notifyDemo();
+    }
 }
